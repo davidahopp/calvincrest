57697c9ef745988993d7b400f86b194098d16324
David Hopp <david.hopp@agapered.com>
2013-02-24 07:06:43 +0000

    adding inquires gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,6 @@ Refinery::Pages::Engine.load_seed
 
 # Added by Refinery CMS Blog engine
 #Refinery::Blog::Engine.load_seed
+
+# Added by Refinery CMS Inquiries engine
+Refinery::Inquiries::Engine.load_seed
