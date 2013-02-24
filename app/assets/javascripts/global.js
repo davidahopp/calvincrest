00a14bffcd0e908014a420ff3f4959d527bc0cd2
David Hopp <david.hopp@agapered.com>
2013-02-24 05:10:18 +0000

    adding left nav abilities

---
+++ app/assets/javascripts/global.js
@@ -0,0 +1,13 @@
+$(function(){
+
+
+  $('.left-nav .menu li').mouseover(function(){
+    $(this).addClass('active')
+  })
+  $('.left-nav .menu li').mouseout(function(){
+    $(this).removeClass('active')
+  })
+
+
+
+})
