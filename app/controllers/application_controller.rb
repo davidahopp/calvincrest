5a7f33cf0706b984841f3f62e8997bab0ffea8f1
David Hopp <david.hopp@agapered.com>
2013-02-25 03:53:36 +0000

    adding custom 500 error page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from Exception, :with => :render_500
+
+  private
+
+  def render_500(exception)
+    render template: 'shared/500.html', status: 500
+  end
+
 end
